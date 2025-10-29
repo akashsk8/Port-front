99bbd2186cf52106c4ee57a65e51cefe9fd65067
JUMMAN SK <92851033+akashsk8@users.noreply.github.com>
2025-10-29 14:12:46 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   allowedDevOrigins :['http://131.108.196:3000','https://port-back-hazel.vercel.app']
+   allowedDevOrigins :['https://port-back-chi.vercel.app']
 };
 export default nextConfig;
 
