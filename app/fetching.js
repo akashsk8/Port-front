7b4a71f676055b2813c39a72a1e2b013d68bb684
JUMMAN SK <92851033+akashsk8@users.noreply.github.com>
2025-10-29 05:33:17 +0000

    Update fetching.js

--- app/fetching.js
+++ app/fetching.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const Api_Url = "http://10.131.108.196:5000/Java";
+const Api_Url = "https://port-back-hazel.vercel.app/Java";
 async function fetchData() {
     try {
       const response = await axios.get(Api_Url);
